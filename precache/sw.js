540058701dd566aaba2b7245d0a5f37e23acd913
Simon Eastwood <seastwood@epo.org>
2019-10-04 15:08:08 +0200

    add slow ajax call

--- precache/sw.js
+++ precache/sw.js
@@ -1,6 +1,11 @@
 
 
 
-self.addEventListener('fetch', function(event) {
-				return fetch(event.request);
-			});
+self.addEventListener('fetch', function (event) {
+	return fetch(event.request);
+});
+
+
+addEventListener('activate', (event) => {
+	console.log('[Service Worker] Activate Event ', event)
+})
