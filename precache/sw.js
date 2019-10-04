3485567892bff396f79fc0bda17a149868ba838b
Simon Eastwood <seastwood@epo.org>
2019-10-04 15:18:28 +0200

    added console log

--- precache/sw.js
+++ precache/sw.js
@@ -3,6 +3,7 @@
 
 self.addEventListener('fetch', function (event) {
 	if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
+	console.log('[Service Worker] Fetching');
 	return fetch(event.request);
 });
 
