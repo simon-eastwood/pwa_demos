f1360a8127223f1e22112d336a58b047b167080c
Simon Eastwood <seastwood@epo.org>
2019-10-04 16:31:12 +0200

    add ccache open

--- precache/sw.js
+++ precache/sw.js
@@ -9,29 +9,32 @@ var urlsToPrefetchOnSpaStartup = [
 
 
 function prefetch(urlList) {
-	var cachePromises = urlList.map(function (urlToPrefetch) {
-		// This constructs a new URL object using the service worker's script location as the base
-		// for relative URLs.
-		var url = new URL(urlToPrefetch, location.href);
-		// Append a cache-bust=TIMESTAMP URL parameter to each URL's query string.
-		// This is particularly important when precaching resources that are later used in the
-		// fetch handler as responses directly, without consulting the network (i.e. cache-first).
-		// If we were to get back a response from the HTTP browser cache for this precaching request
-		// then that stale response would be used indefinitely, or at least until the next time
-		// the service worker script changes triggering the install flow.
-		//url.search += (url.search ? '&' : '?') + 'cache-bust=' + now;
+	var cachePromises = caches.open(CURRENT_CACHES.prefetch).then(function (cache) {
 
-		var request = new Request(url, {});
-		return fetch(request).then(function (response) {
-			if (response.status >= 400) {
-				throw new Error('request for ' + urlToPrefetch +
-					' failed with status ' + response.statusText);
-			}
+		urlList.map(function (urlToPrefetch) {
+			// This constructs a new URL object using the service worker's script location as the base
+			// for relative URLs.
+			var url = new URL(urlToPrefetch, location.href);
+			// Append a cache-bust=TIMESTAMP URL parameter to each URL's query string.
+			// This is particularly important when precaching resources that are later used in the
+			// fetch handler as responses directly, without consulting the network (i.e. cache-first).
+			// If we were to get back a response from the HTTP browser cache for this precaching request
+			// then that stale response would be used indefinitely, or at least until the next time
+			// the service worker script changes triggering the install flow.
+			//url.search += (url.search ? '&' : '?') + 'cache-bust=' + now;
 
-			// Use the original URL without the cache-busting parameter as the key for cache.put().
-			return cache.put(urlsToPrefetchOnSpaStartup, response);
-		}).catch(function (error) {
-			console.error('Not caching ' + urlToPrefetch + ' due to ' + error);
+			var request = new Request(url, {});
+			return fetch(request).then(function (response) {
+				if (response.status >= 400) {
+					throw new Error('request for ' + urlToPrefetch +
+						' failed with status ' + response.statusText);
+				}
+
+				// Use the original URL without the cache-busting parameter as the key for cache.put().
+				return cache.put(urlsToPrefetchOnSpaStartup, response);
+			}).catch(function (error) {
+				console.error('Not caching ' + urlToPrefetch + ' due to ' + error);
+			});
 		});
 	});
 
@@ -49,7 +52,7 @@ self.addEventListener('fetch', function (event) {
 
 	if (requestURL.pathname.endsWith('.webmanifest')) {
 		console.log('[Service Worker] SPA is starting up');
-		prefetch (urlsToPrefetchOnSpaStartup);
+		prefetch(urlsToPrefetchOnSpaStartup);
 	}
 	console.log('[Service Worker] Doing Fetch');
 	return fetch(event.request);
