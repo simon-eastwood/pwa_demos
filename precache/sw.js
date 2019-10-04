9d08e370e5e84542e5ca1a8258dc70afe7e0fc86
Simon Eastwood <seastwood@epo.org>
2019-10-04 15:53:26 +0200

    detect SPA startup

--- precache/sw.js
+++ precache/sw.js
@@ -5,6 +5,10 @@ self.addEventListener('fetch', function (event) {
 	const requestURL = new URL(event.request.url);
 	console.log('[Service Worker] Fetch requested for ' +requestURL.pathname);
 	if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
+
+	if (requestURL.pathname.endsWith('.webmanifest')) {
+		console.log ('[Service Worker] SPA is starting up');
+	}
 	console.log('[Service Worker] Doing Fetch');
 	return fetch(event.request);
 });
